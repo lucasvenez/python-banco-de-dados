4fe230767eadf89f28428ae96b21f2c223e67cc1
Lucas Venezian Povoa <lucasvenez@gmail.com>
2025-05-24 11:20:35 -0300

    Add questions

--- vendas.perguntas.dql.sql
+++ vendas.perguntas.dql.sql
@@ -40,5 +40,38 @@ FROM
 -- }
 
 -- SOLUÇÃO COM WITH
-WITH 
+WITH SEM_DESCONTO AS (
+    SELECT COUNT(*) AS 'SEM' 
+    FROM Venda 
+    WHERE taxaDesconto = 0
+),
+COM_DESCONTO AS (
+    SELECT COUNT(*) AS 'COM' 
+    FROM Venda 
+    WHERE taxaDesconto > 0
+)
+SELECT *
+FROM SEM_DESCONTO, COM_DESCONTO;
 
+-- Quais as vendas com valor total superior a R$ 100,00?
+SELECT 
+    V.idVenda, 
+    V.dataHora, 
+    ROUND(SUM(I.quantidade * I.precoUnitario), 2) AS valorTotal
+FROM 
+    Venda AS V INNER JOIN 
+    ItemVenda AS I ON (V.idVenda = I.idVenda)
+GROUP BY 
+    idVenda, 
+    dataHora
+-- HAVING valorTotal > 100
+ORDER BY idVenda;
+
+SELECT 
+    V.idVenda, 
+    V.dataHora, 
+    quantidade, precoUnitario,
+    ROUND(I.quantidade * I.precoUnitario, 2) AS valorTotalItem
+FROM 
+    Venda AS V INNER JOIN 
+    ItemVenda AS I ON (V.idVenda = I.idVenda)
